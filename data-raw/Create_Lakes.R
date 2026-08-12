142c68977c3cba9c7d5266902db0e0efc0149e5d
Paul Askey <paul.askey@gofishbc.com>
2026-08-12 10:23:27 -0700

    Updated Lakes data

--- data-raw/Create_Lakes.R
+++ data-raw/Create_Lakes.R
@@ -6,7 +6,9 @@ DP2R::DP2R(Tables = c("vwWaterbodyLake","vwWaterbody"))
 Lakes = dplyr::full_join(vwWaterbodyLake, vwWaterbody, by = c("WBID","gazetted_name"))
 
 
-cols_order = c("waterbody_type", "region_code", "WBID", "gazetted_name", "alias", "locale_name", "nearest_town",  "area_ha", "lake_volume_m3", "max_depth_m", "mean_depth_m", "elevation_m",
+cols_order =
+  c("waterbody_type", "region_code", "WBID", "gazetted_name", "alias", "locale_name", "nearest_town",
+  "area_ha", "lake_volume_m3", "max_depth_m", "mean_depth_m", "elevation_m",
   "perimeter", "littoral_area_ha", "littoral_area_percent", "max_water_level",
   "num_outlets", "num_perm_inlets", "4wd",
   "hike_in", "boat_launch", "fishing_pier", "campsite",
